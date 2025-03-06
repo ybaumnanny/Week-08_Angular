b2d998c4ef1a73b6274a7ad713643514dce0f4c5
Yaman Mahtha <yamanmahtha@gmail.com>
2025-03-06 13:39:35 +0530

    [YAMAN] add. Added the ability to surf to the BridgeLabz website by clicking the LOGO

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -7,9 +7,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello From Bridgelabz!!!👋🏻';
   imageUrl = "assets/blz.png";
+  url = "https://www.bridgelabz.com/";
+  name = "Bridgelabz";
 
   ngOnInit(): void{
     this.title = "Hello From Bridgelabz!!!👋🏻";
-
   }
+
+  onClick($event: MouseEvent): void{
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+
 }
