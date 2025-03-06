c267999bf8a4cb475488926940e7f39219b48c61
Yaman Mahtha <yamanmahtha@gmail.com>
2025-03-06 12:07:03 +0530

    [YAMAN] add.Create a Helloworld Angular App to display [Hello from BridgeLabz!!!] using data bindings.

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  title = 'Hello From Bridgelabz!!!👋🏻';
+
+  ngOnInit(): void{
+    this.title = "Hello From Bridgelabz!!!👋🏻";
+
+  }
 }
